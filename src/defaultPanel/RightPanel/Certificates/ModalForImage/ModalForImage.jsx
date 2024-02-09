7b34842e1de8c43762363561a5f84593bda662c0
Korneliia <korneliiaPositive@gmail.com>
2024-02-09 17:15:45 +0100

    changes

---
+++ src/defaultPanel/RightPanel/Certificates/ModalForImage/ModalForImage.jsx
@@ -0,0 +1,20 @@
+import style from "./ModalForImage.module.css";
+
+const ModalForImage = (props) => {
+    const dataFunction = props.dataFunction;
+    const imgSrc = props.dataSrc;
+    const altText = props.dataAlt;
+    return (
+        <>
+            <div className={style.container}>
+            <span className={style.close} onClick={() => {
+                dataFunction(false)
+            }}>X
+            </span>
+                <img src={imgSrc} alt={altText}/>
+            </div>
+            <div className={style.backgroundWhichBlock}></div>
+        </>
+    )
+}
+export default ModalForImage;
